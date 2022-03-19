66728a941c4be8a23a213f00fcdd894d2ef5b614
Masnawi Rahmat <97861822+masnawi-rahmat@users.noreply.github.com>
2022-03-19 23:48:41 +0800

    Create local.tf

---
+++ local.tf
@@ -0,0 +1,12 @@
+resource "local_file" "lcd_tv_living_room" {
+  filename = "/home/your_user_name/company_x/ceo_office/head.txt"
+  content = "mr susse"
+}
+resource "local_file" "lcd_tv_master_bedroom" {
+  filename = "/home/your_user_name/company_x/it_department/head.txt"
+  content = "mr raspbian"
+}
+resource "local_file" "lcd_tv_guest_bedroom" {
+  filename = "/home/Your_user_name/company_x/hr_department/head.txt"
+  content = "miss fedora"
+}
