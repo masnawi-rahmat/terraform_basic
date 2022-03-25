05689338b4b74e46a8000e072a89b2d13e5a487a
Masnawi Rahmat <masnawi.rahmat@gmail.com>
2022-03-26 07:35:30 +0800

    Update local.tf

--- local.tf
+++ local.tf
@@ -3,9 +3,9 @@
 # in Generation SG/Temasek Poly SGUPMSCT Cloud Support and DevOps Bootcamp cohort no 4 Yr 2022
 # this terraform is created with learning purpose more than optimisation in mind
 # this terraform code creates an 'imaginary' company (company_x) and its departments as directory and subdirectories with department heads named in respective txt files
-# objectives of this terraform code is to give you the basic understanding of how terraform works by invoking the local_file resource function by:
-# (1) creating directory and subdirectories in your local machine
-# (2) creating txt files (with contents) in the subdirectories
+# objectives of this terraform code is to give you the basic understanding of how terraform works using the 'local_file' resource to:
+# (1) create directory and subdirectories in your local machine
+# (2) create txt files with contents in the subdirectories
 # please substitute 'your_user_name' with your own user name
 # go ahead and change the the directory/subdirectries name and contents of the txt files; and apply and re-apply the terraform code and see what happen
 
